b87313ca23c643d96d36ec0a804ef84f8c0cb19c
MathijsKeenens <11501101@Student.pxl.be>
2017-04-21 15:33:20 +0200

    WerkPakket 2
    ef

---
+++ MonkyBusiness/application/models/PeopleModel.php
@@ -0,0 +1,66 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class peopleModel extends CI_Model {
+
+	public function getPeoples()
+	{
+		$this->db->select("*");
+		$this->db->from('Persons');
+		
+		$query = $this->db->get();
+
+		return $query->result();
+		/*
+		$num_data_returned = $query->num_rows;
+		
+		if ($num_data_returned < 1) {
+			
+			echo "There is no data in the database";
+			exit(); }
+		*/
+	}
+	
+	public function insertPerson($name, $address, $telephone) {
+		
+		$this->db->set('name', $name);
+		$this->db->set('address', $address);
+		$this->db->set('telephone', $telephone);
+		$this->db->insert('Person');
+	}
+	
+	public function deletePerson($personID) {
+		$this->db->where('personID', $personID);
+		$this->db->delete('Person');
+	}
+	
+	public function getPerson($personID) {
+         
+		 $this->db->where('personID', $personID);
+		 $query = $this->db->get('Person');
+		 
+
+
+		$result = $query->result();
+		
+		foreach ($result as $row) {
+			
+			$users[$row->personID] = array($row->name, $row->address, $row->telephone);
+            return $users->result();
+        }
+
+		 }
+		 
+
+	
+	
+		public function updatePerson($personID, $name, $address, $telephone) {
+		
+		$this->db->where('personID', $personID);
+		$this->db->set('name', $name);
+		$this->db->set('address', $address);
+		$this->db->set('telephone', $telephone);
+		$this->db->update('Person');
+	}
+	
+	
+}
